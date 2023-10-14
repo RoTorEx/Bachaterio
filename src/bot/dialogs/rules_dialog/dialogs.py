72918658322303c0b04d226fb280cf7fa31c9b11
Aleksey Strelkov <aleks.strel.8v@gmail.com>
2023-10-14 14:38:52 +0200

    feat(rules): add main rules

--- src/bot/dialogs/rules_dialog/dialogs.py
+++ src/bot/dialogs/rules_dialog/dialogs.py
@@ -9,7 +9,16 @@ from .handlers import return_to_menu
 
 rules_dialog = Dialog(
     Window(
-        Const("It's empty in here!"),
+        Const(
+            "1. The first rule of Bachata Club is: You do not talk about Bachata Club.\n"
+            "2. The second rule of Bachata Club is: You DO NOT talk about Bachata Club.\n"
+            "3. If someone says `Stop` or signals to end the dance, the dance is over.\n"
+            "4. Only two dancers in a couple at a time.\n"
+            "5. One dance at a time.\n"
+            "6. No restrictive clothing, and show respect for your dance partner.\n"
+            "7. Dances can go on as long as they have to, even if it lasts all night.\n"
+            "8. If this is your first time at Bachata Club, you have to dance.\n"
+        ),
         Button(
             Const("Back to menu ↩"),
             id="return_to_main_menu",
